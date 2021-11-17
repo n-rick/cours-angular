3f97bceb5710a8f8147627e886da057dba4f9339
n-rick <e.ficadiere@gmail.com>
2021-11-18 00:07:21 +0100

    modif composant adresse ajout liens

--- src/app/composants/adresse/adresse.component.ts
+++ src/app/composants/adresse/adresse.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-adresse',
@@ -10,20 +10,24 @@ export class AdresseComponent implements OnInit {
   rue = "";
   ville = "";
   codePostal = "";
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
-  // ngOnInit(): void {
-  //   this.route.queryParamMap.subscribe(
-  //   (resp) => {
-  //     this.rue = resp.get('rue') ?? "";
-  //     this.ville = resp.get('ville') ?? "";
-  //     this.codePostal = resp.get('codePostal') ?? "";
-  //     }
-  //   )
-  // }
   ngOnInit(): void {
-      this.rue = this.route.snapshot.queryParams['rue'] ?? " 30 avenue des Champs";
-      this.ville = this.route.snapshot.queryParams['ville'] ?? "Aix-en-Provence";
-      this.codePostal = this.route.snapshot.queryParams['codePostal'] ?? "13100";
+    this.route.queryParamMap.subscribe(
+    (resp) => {
+      this.rue = resp.get('rue') ?? "";
+      this.ville = resp.get('ville') ?? "";
+      this.codePostal = resp.get('codePostal') ?? "";
+      }
+    )
+  }
+  // ngOnInit(): void {
+  //     this.rue = this.route.snapshot.queryParams['rue'] ?? " 30 avenue des Champs";
+  //     this.ville = this.route.snapshot.queryParams['ville'] ?? "Aix-en-Provence";
+  //     this.codePostal = this.route.snapshot.queryParams['codePostal'] ?? "13100";
+  // }
+
+  goToStagiaire(){
+    this.router.navigate(['/stagiaire', "messi", "lionel"]);
   }
 }
