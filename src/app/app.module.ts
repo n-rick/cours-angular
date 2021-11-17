94163051018bc0bd8098fe742d3b9b43f945e154
n-rick <e.ficadiere@gmail.com>
2021-11-17 15:52:03 +0100

    ajout menu sur home page & router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservableComponent } from './composants/observable/observable.component';
+import { AdresseComponent } from './composants/adresse/adresse.component';
+import { StagiaireComponent } from './composants/stagiaire/stagiaire.component';
+import { MenuComponent } from './composants/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservableComponent,
+    AdresseComponent,
+    StagiaireComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
