2f12dbdae133aabdc2322d9a9b88ca0163fe90cf
n-rick <e.ficadiere@gmail.com>
2021-11-17 15:51:15 +0100

    creation du routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdresseComponent } from './composants/adresse/adresse.component';
+import { ObservableComponent } from './composants/observable/observable.component';
+import { StagiaireComponent } from './composants/stagiaire/stagiaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'adresse', component: AdresseComponent},
+  {path: 'stagiaire', component: StagiaireComponent},
+  {path: 'stagiaire/:nom/:prenom', component: StagiaireComponent},
+  {path: 'observable', component: ObservableComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
